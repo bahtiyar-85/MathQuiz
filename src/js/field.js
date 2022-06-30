b266eac2e8209bdda1d9d72145e13bd74ecae3d1
bahtiyar-85 <b.ilyazov@gmail.com>
2022-06-30 13:50:14 +0600

    add script for main page

---
+++ src/js/field.js
@@ -0,0 +1,2 @@
+import config from "/src/data/config.json" assert {type: "json"};
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,38 @@
-const getPlayerName = () => {
-    const name = localStorage.getItem('playerName') || "player"
+export const getPlayerFromLocalStorage = () => {
+   return localStorage.getItem('playerName') || "player"
+}
+
+const setPlayerToLocalStorage = (value) => {
+    localStorage.setItem('playerName', value)
+}
+
+const setPlayerName = () => {
+    const name = getPlayerFromLocalStorage()
     const inputName = document.querySelector("#name")
-    console.log(name);
     inputName.value = name;
     inputName.focus()
 }
 
+const checkInputName = (input) => {
+    return input.value ? true : false
+}
+
+export const changeActiveWindow = (from, to) => {
+    document.querySelector(`.${from}`).classList.remove("block_active")
+    document.querySelector(`.${to}`).classList.add("block_active")
+}
+
+const handleGoBtn = () => {
+    const inputName = document.querySelector("#name")
+    if(checkInputName(inputName)){
+        setPlayerToLocalStorage(inputName.value)
+        changeActiveWindow('main', 'field')
+    } else {
+        inputName.focus();
+    }
+}
+
 export const initGame = () => {
-    getPlayerName();
+    setPlayerName();
+    document.querySelector('.main__btn').addEventListener('click', handleGoBtn)
 }
